68f1b7e455a79680c5fa85543ee4a75a44a8c4a3
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-07-18 08:52:16 +0300

    finishh

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,42 @@
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-const input = document.querySelector('#controls input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#boxes');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const elAmount = document.querySelector('input[type="number"]');
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  if (elAmount.value >= 1 && elAmount.value <= 100) {
+    createBoxes(elAmount.value);
+    elAmount.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
 
 function createBoxes(amount) {
-
-  boxesContainer.innerHTML = '';
+  boxes.innerHTML = "";
 
   let size = 30;
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    fragment.appendChild(div);
+
     size += 10;
   }
+
+  boxes.append(fragment);
 }
 
-createButton.addEventListener('click', function() {
-  const amount = parseInt(input.value);
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    input.value = '';
-  } else {
-    alert('Please enter a number between 1 and 100.');
-  }
-});
-
-destroyButton.addEventListener('click', function() {
-  boxesContainer.innerHTML = '';
-});
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
